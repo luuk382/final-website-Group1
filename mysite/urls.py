f29ce376d763c80c7038f2c977291c17a163b0b2
valeriyaprokopova <valeriya1625@gmail.com>
2017-12-17 12:42:37 +0100

    The final stage of DjangoGirlTutorial

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
+
+
