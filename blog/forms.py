8c398f219491819df45d3d06f3bc16a60e32b84c
valeriyaprokopova <valeriya1625@gmail.com>
2017-12-18 23:23:14 +0100

    added previous layout

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text','image',)
+        fields = ('title', 'description', 'text', 'image', 'cookingtime', 'difficulty', )
 
 class CommentForm(forms.ModelForm):
 
--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 from django.utils import timezone
 import os
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200) #title can be the max of 200 characters 
     text = models.TextField()
+    description = models.TextField(max_length=100, blank=False, null=True)
     image = models.ImageField(upload_to='post_image', blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    
+    text = models.TextField()
+    DIFFICULTY_LEVELS = (
+        ('Easy', 'Easy'),
+        ('Medium', 'Medium'),
+        ('Difficult', 'Difficult'),
+    )
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_LEVELS, blank = False)
+    hours = models.IntegerField(default=0)
+    minutes = models.IntegerField(default=0)
+    cookingtime = models.CharField(default=0, max_length=3, help_text="Please enter time in minutes", blank = False)
+
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
